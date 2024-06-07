7d42909360ca588e5b322ec941470a254929f515
mferste <mferste@yahoo.com>
2024-06-07 18:27:37 -0300

    primeiro commit

---
+++ src/main/java/aplicacao/soma.java
@@ -0,0 +1,7 @@
+package aplicacao;
+
+public class soma {
+	public double soma(double n1, double n2) {
+		return n1 + n2;
+	}
+}
---
+++ src/test/java/teste/somaTeste.java
@@ -0,0 +1,19 @@
+package teste;
+
+import org.junit.Test;
+
+import aplicacao.soma;
+import junit.framework.Assert;
+
+public class somaTeste {
+	
+	@Test
+	public void testeSoma() {
+		double result;
+		
+		soma conta = new soma();
+		result = conta.soma(10.0, 5.0);
+		
+		Assert.assertEquals(10.0, result, 0);
+	}
+}
